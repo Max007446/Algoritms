bb17757efa01c815b560031435394afefb8a70a7
max007446 <max007446@gmail.com>
2021-02-09 22:46:43 +0300

    ostatok

---
+++ lesson1/ostatok/ostatok.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var N, K, ch, ost int
+
+	fmt.Printf("Введите N: \n")
+	fmt.Scanln(&N)
+	fmt.Printf("Введите K: \n")
+	fmt.Scanln(&K)
+	for {
+		N = N - K
+		ch = ch + 1
+		if N <= K {
+			break
+		}
+	}
+	ost = N
+	fmt.Println("Частное = ", ch)
+	fmt.Println("Остаток от деления = ", ost)
+}
